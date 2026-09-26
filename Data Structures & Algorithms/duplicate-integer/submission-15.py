b3da51ad77f0a2ee4bf70955a286c54593c4e025
Soham Maitra <49953213+soham1wnl@users.noreply.github.com>
2026-09-26 14:41:05 +0530

    Remove: duplicate-integer - submission-15

--- Data Structures & Algorithms/duplicate-integer/submission-15.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, dick: List[int]) -> bool:
-        sex = set()
-        for semen in dick:
-            if semen in sex:
-                return True
-            sex.add(semen)
-        return False
